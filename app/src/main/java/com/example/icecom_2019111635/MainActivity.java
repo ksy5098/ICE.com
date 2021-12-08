b3d79bd32a7f83759f8de022f8021ebe7e28fe3c
DESKTOP-41B437G\82103 <seoyoung4228@gmail.com>
2021-12-08 17:43:51 +0900

    call, email

--- app/src/main/java/com/example/icecom_2019111635/MainActivity.java
+++ app/src/main/java/com/example/icecom_2019111635/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.icecom_2019111635;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button show_product = findViewById(R.id.show_product);
+        TextView call = findViewById(R.id.call);
+        TextView email = findViewById(R.id.email);
+        TextView location = findViewById(R.id.location);
+
+
+        show_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:01036325098"));
+                startActivity(intent);
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("mailto:"));
+                intent.setType("plain/text");
+                String[] address = {"seoyoung4228@naver.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, address);
+                startActivity(intent);
+            }
+        });
+
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setData(Uri.parse("mailto:"));
+                intent.setType("plain/text");
+                String[] address = {"seoyoung4228@naver.com"};
+                intent.putExtra(Intent.EXTRA_EMAIL, address);
+                startActivity(intent);
+            }
+        });
     }
+
 }
