195eaf4fd6e0e8b59eca9c405e7e29b230a2750d
DESKTOP-41B437G\82103 <seoyoung4228@gmail.com>
2021-12-12 19:18:02 +0900

    activity_customer_service.xml 수정

---
+++ app/build/generated/source/buildConfig/debug/com/example/icecom_2019111635/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.icecom_2019111635;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.icecom_2019111635";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
